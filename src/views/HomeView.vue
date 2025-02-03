0584953d2569ae75cfb28d1d7e568983af027c91
Jose6348 <jfalasco88@gmail.com>
2025-02-02 22:46:17 -0300

    style

---
+++ src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="home">
+      <App />
+    </div>
+  </template>
+  
+  <script>
+  import App from '@/App.vue'
+  
+  export default {
+    name: 'HomeView',
+    components: {
+      App
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* Estilos específicos para a HomeView podem ser adicionados aqui */
+  </style>
